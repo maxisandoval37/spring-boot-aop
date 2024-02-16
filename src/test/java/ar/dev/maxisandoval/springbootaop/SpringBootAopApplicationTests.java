4f7fe920dcbda1de7114b6bed218ba8cbc9882bb
Maxi Sandoval <maxisandoval98@gmail.com>
2024-02-15 21:27:14 -0300

    primer commit

---
+++ src/test/java/ar/dev/maxisandoval/springbootaop/SpringBootAopApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.springbootaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAopApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
